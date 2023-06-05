7bfa35d4f1da42e462a9132b197372c74eebc3fc
mo-amri <96189386+mo-amri@users.noreply.github.com>
2023-06-05 16:14:45 +0100

    Update streamlit_app.py
    Version 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 
-from io import StringIO
-
 import streamlit as st
 import csv
 import time
@@ -9,12 +7,12 @@ from streamlit import session_state
 
 
 def monitoring_trading():
+    # Set the page title and layout
+    st.set_page_config(page_title='Amri Investment Web Application', layout='centered')  # wide
 
-    # st.title("Amri Investment Web Application")
-    st.markdown("<h1 style='color: salmon;'>Amri Investment Web Application</h1>", unsafe_allow_html=True)
+    st.title("Amri Investment Web Application")
     st.subheader("Monitoring Trading Activity")
 
-    # st.sidebar.markdown("<h1 style='color: salmon;'>Amri Investment Ltd</h1>", unsafe_allow_html=True)
     st.sidebar.subheader("London Stock Exchange")
 
     with st.sidebar.form("calculater_form"):
@@ -25,70 +23,104 @@ def monitoring_trading():
             processing_date = st.date_input("Processing Date")
         with col2:
             processing_time = st.time_input("Processing Time")
+
         company_name = st.text_input("Trading Company Name:")
-        trading_budget = st.number_input("Trading Budget GBP", min_value=0.0)
+
+        col1, col2 = st.columns(2)
+        with col1:
+            trading_budget = st.number_input("Trading Budget GBP", min_value=0.0)
+        with col2:
+            trading_fees = st.number_input("Trading Fees GBP", min_value=0.0)
+
         # ___Create four columns
-        col3, col4 = st.columns(2)
-        with col3:
+        col1, col2 = st.columns(2)
+        with col1:
             # ----------------------------------------"Please enter the Purchase Price per Share in the input field."
             purchase_price_per_share = st.number_input("Purchase Share Price GBX", min_value=0.0)
-        with col4:
+        with col2:
             # ----------------------------"Enter The Time at Which The purchase Price Occurred"
             purchase_time = st.text_input("Purchase Time")
-        col5, col6 = st.columns(2)
-        with col5:
+        col1, col2 = st.columns(2)
+        with col1:
             selling_price_per_share = st.number_input('Selling Share Price GBX', min_value=0.0)
-        with col6:
+        with col2:
             selling_time = st.text_input("Selling Time")
 
         # Add a submit button
-        submit_button = st.form_submit_button("Submit")
+        submit_button = st.form_submit_button("Calculate The Profit / Loss")
 
         if submit_button:
-            # Process the form data
-            # You can perform any necessary operations with the form data here
-            # For example, you can store it in a database or display it on the screen
-            st.success("Form Submitted!")
-            st.write("Processing Date", processing_date.strftime('%d-%m-%Y'), "Processing Time",  processing_time)
-            st.write("Company Name:", company_name)
-            st.write("Trading Budget:", trading_budget)
-            st.write("Purchase Price per Share:", purchase_price_per_share, "Purchase Time:", purchase_time)
-            st.write("Selling Price per Share:", selling_price_per_share, "Selling Time:", selling_time)
+            # ___ Process the form data & Calculate the trading profit / loss
+            # ___ Convert purchase price from GBX to GBP.
+            gbp_purchase = purchase_price_per_share / 100
+            # ___ Convert selling price from GBX to GBP.
+            gbp_selling = selling_price_per_share / 100
 
-    # _____ - Calculate & Display the Data. __________________________________________________________________________
-    if st.button("Calculate The Profit or Loss"):
+            amount_of_shares = trading_budget / gbp_purchase
+            gross_amount = amount_of_shares * gbp_selling
+            profit_or_lost_made = gross_amount - (trading_budget + trading_fees)
 
-        # Create progress bar
-        progress_bar = st.progress(0)
-        # Update the progress bar
-        for percent_complete in range(100):
-            time.sleep(0.01)  # Simulate some computation
-            progress_bar.progress(percent_complete + 1)
+            # Check up the result
+            if profit_or_lost_made == 0:
+                st.info(f"NO Profit Made:  £{round(profit_or_lost_made, 2)}")
+            elif profit_or_lost_made > 0:
+                st.success(f"Profit Made:  £{round(profit_or_lost_made, 2)}")
+            else:
+                st.warning(f"Made a loss:  £{round(profit_or_lost_made, 2)}")
 
-        # Calculate the trading profit or lost
+            # Display the submitted user inputs
+            st.write(f"Amount of Shares: {round(amount_of_shares, 2)}")
+            st.write(f"Gross Amount: £{round(gross_amount, 2)}")
+
+    # _____ - Display the Data. __________________________________________________________________________
+    if st.button("Display The Data"):
+
+        # Calculate the trading profit or loss
         amount_of_shares = trading_budget / purchase_price_per_share
         gross_amount = amount_of_shares * selling_price_per_share
         profit_or_lost_made = gross_amount - trading_budget
 
-        # Display the submitted user inputs
-        # st.write(f"Trading Date: {processing_date.strftime('%d-%m-%Y')}, Processing Time: {processing_time}")
-        st.write(f"Company Name: {company_name}")
-        st.write(f"Trading Budget: £{round(trading_budget, 2)}")
-        # st.write(f"Purchase Price per Share: {purchase_price_per_share} " f"GBX  - Purchase Time: {purchase_time}")
-        # st.write(f"Selling Price per Share: {selling_price_per_share} " f"GBX  - Selling Time: {selling_time}")
-        st.write(f"Amount of Shares: {round(amount_of_shares, 2)}")
-        st.write(f"Gross Amount: £{round(gross_amount, 2)}")
         # Check up the result
         if profit_or_lost_made == 0:
-            st.info(f" **No Profit Was Made:**  £{round(profit_or_lost_made, 2)}")
+            st.write(f"**NO Profit Made:  £{round(profit_or_lost_made, 2)}**")
         elif profit_or_lost_made > 0:
-            st.success(f" **Profit Made:**  £{round(profit_or_lost_made, 2)}")
-            st.snow()
+            st.write(f"**Profit Made:  £{round(profit_or_lost_made, 2)}**")
         else:
-            st.warning(f" **Made a loos:**  £ {round(profit_or_lost_made, 2)}")
+            st.write(f"**Made a loos:  £{round(profit_or_lost_made, 2)}**")
+
+        col1, col2, col = st.columns(3)
+        with col1:
+            st.write(f"Trading Date: **{processing_date.strftime('%d-%m-%Y')}**")
+        with col2:
+            st.write(f"Processing Time: **{processing_time}**")
+
+        col1, col2, col = st.columns(3)
+        with col1:
+            st.write(f"Company Name: **{company_name}**")
+        with col2:
+            st.write(f"Trading Budget: **£{round(trading_budget, 2)}**")
+
+        col1, col2, col = st.columns(3)
+        with col1:
+            st.write(f"Price per Share: **{purchase_price_per_share} GBX**")
+        with col2:
+            st.write(f"Purchase Time: **{purchase_time}**")
+
+        col1, col2, col = st.columns(3)
+        with col1:
+            st.write(f"Selling Price per Share: **{selling_price_per_share} GBX**")
+        with col2:
+            st.write(f"Selling Time: **{selling_time}**")
+
+        col1, col2, col = st.columns(3)
+        with col1:
+            st.write(f"Amount of Shares: **{round(amount_of_shares, 2)}**")
+        with col2:
+            st.write(f"Gross Amount: **£{round(gross_amount, 2)}**")
 
     # _____ - Display The Data as a Table. ________________________________________________________________________
-    if st.button("Display The Data as a Table"):
+    if st.button("Display The Table Data"):
+
         # Calculate the trading profit or lost
         amount_of_shares = trading_budget / purchase_price_per_share
         gross_amount = amount_of_shares * selling_price_per_share
@@ -107,38 +139,11 @@ def monitoring_trading():
 
         # Display the table
         st.table(df)
-        # st.dataframe(df)
-
-    # # +++++++++++++++++++++++++++++ Create a CSV file & Download to the local directory +++++++++++++++++++++++++++
-    # if st.button("Download the Data Table to Local Directory"):
-    #     # Calculate the trading profit or lost
-    #     amount_of_shares = trading_budget / purchase_price_per_share
-    #     gross_amount = amount_of_shares * selling_price_per_share
-    #     profit_or_lost_made = gross_amount - trading_budget
-    #
-    #     # # Define the data to be written to the CSV file
-    #     data = [
-    #         ['Processing_Date', 'Processing_Time', 'Company_Name', 'Trading_Budget', 'Purchase_Price_per_Share',
-    #          'Purchase Time', 'Selling_Price_per_Share', 'Selling_Time', 'Amount_of_Shares', 'Gross_Amount',
-    #          'Profit_Made'],
-    #         [processing_date.strftime('%d-%m-%Y'), processing_time, company_name, round(trading_budget, 2),
-    #          round(purchase_price_per_share, 2), purchase_time, round(selling_price_per_share, 2),
-    #          selling_time, round(amount_of_shares, 2), round(gross_amount, 2), round(profit_or_lost_made, 2)]]
-    #
-    #     # Specify the file path for the CSV file
-    #     file_path = f"Data/ {company_name} .csv"
-    #
-    #     # Write the data to the CSV file
-    #     with open(file_path, 'w', newline='') as csvfile:
-    #         writer = csv.writer(csvfile)
-    #         writer.writerows(data)
-    #
-    #     # Provide a success message to the user
-    #     st.success(f"CSV File Downloaded Successfully!")
 
     # ____ - Download CSV File.  ____________________________________________________________________________________
     # Generate a download button
-    if st.button("Download Table as CSV File"):
+    if st.button("Download CSV File"):
+
         # Calculate the trading profit or lost
         amount_of_shares = trading_budget / purchase_price_per_share
         gross_amount = amount_of_shares * selling_price_per_share
@@ -190,7 +195,7 @@ def process_data_gbx_to_gbp(gbp):
 
 
 def converter_gbx_to_gbp():
-    st.sidebar.subheader("Convert GBX to GBP")
+    st.sidebar.subheader("GBX to GBP Converter")
 
     with st.sidebar.form("my_form_gbx_to_gb"):
         # st.header("GBX to GBP Converter")
@@ -220,7 +225,7 @@ def process_data_gbp_to_gbx(gbx):
 
 
 def converter_gbp_to_gbx():
-    st.sidebar.subheader("Convert GBP to GBX")
+    st.sidebar.subheader("GBP to GBX Converter")
 
     with st.sidebar.form("my_form_gbp_to_gbx"):
         # st.header("GBX to GBP Converter")
