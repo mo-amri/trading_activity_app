1d2d13e02b7621a2f0fe94e13a7aa09049072509
mo-amri <96189386+mo-amri@users.noreply.github.com>
2023-06-06 19:09:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -213,7 +213,7 @@ def process_data_gbx_to_gbp(gbp):
 def converter_gbx_to_gbp():
     st.sidebar.write("GBX to GBP Converter")
 
-    with st.sidebar.form("my_form_gbx_to_gb")
+    with st.sidebar.form("my_form_gbx_to_gb"):
         gbx = st.number_input("Enter GBX value:")
         if gbx:
             gbx = float(gbx)
